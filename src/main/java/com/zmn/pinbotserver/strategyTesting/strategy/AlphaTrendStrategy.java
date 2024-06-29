7184940a025a00cbe66c546559530a79b04ded96
PinBot <lll24la@gmail.com>
2024-06-29 21:44:23 +0300

    Допил новой стратегию на ATR

--- src/main/java/com/zmn/pinbotserver/strategyTesting/strategy/AlphaTrendStrategy.java
+++ src/main/java/com/zmn/pinbotserver/strategyTesting/strategy/AlphaTrendStrategy.java
@@ -3,17 +3,12 @@ package com.zmn.pinbotserver.strategyTesting.strategy;
 import com.zmn.pinbotserver.historicalData.model.candle.Candle;
 import com.zmn.pinbotserver.strategyTesting.model.order.Order;
 import com.zmn.pinbotserver.strategyTesting.model.order.TYPE;
-import com.zmn.pinbotserver.strategyTesting.model.strategy.StrategyParams;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import com.zmn.pinbotserver.historicalData.model.candle.Candle;
-import com.zmn.pinbotserver.strategyTesting.model.order.Order;
 import com.zmn.pinbotserver.strategyTesting.model.order.Position;
 import com.zmn.pinbotserver.strategyTesting.model.order.STATUS;
-import com.zmn.pinbotserver.strategyTesting.model.order.TYPE;
-import com.zmn.pinbotserver.strategyTesting.model.strategy.StrategyParams;
 import lombok.Getter;
 
 
@@ -21,14 +16,38 @@ public class AlphaTrendStrategy {
 
     private boolean strategyAllowed = true; // Флаг, разрешающий или запрещающий работу стратегии
 
+
+    String tradingPair;
+
+    double currentPrice; // Текущая цена инструмента
+
     // Параметры стратегии
     private double coeff; // Коэффициент мультипликатора для ATR
     private int AP; // Общий период для расчета ATR
+    int LEVERAGE; // Плечо для торговли
+
 
     private double lastAlphaTrend = 0; // Значение AlphaTrend на предыдущем шаге
 
-    private List<Order> openOrders = new ArrayList<>(); // Список открытых ордеров
-    private List<Order> closedOrders = new ArrayList<>(); // Список закрытых ордеров
+
+    @Getter
+    List<Order> orderHistory = new ArrayList<>();
+
+    @Getter
+    List<Position> positionHistory = new ArrayList<>();
+
+    double initialDeposit;
+    double risk;
+
+    double currentDeposit;
+
+    double marginPerOrder;
+    double marginQTY;
+    double minTradingQty;
+
+
+    Position position;
+    int openOrders = 0;
 
     // Конструктор для инициализации параметров стратегии
     public AlphaTrendStrategy(double coeff, int AP) {
@@ -81,20 +100,84 @@ public class AlphaTrendStrategy {
         lastAlphaTrend = AlphaTrend; // Обновление последнего значения AlphaTrend
     }
 
-    // Метод для открытия позиции
-    public void openPosition(TYPE type, double price) {
-        if (!strategyAllowed) return; // Проверка флага разрешения стратегии
+    /**
+     * Метод для расчета маржи на сделку и соответствующего количества монет
+     */
+    private void calculateInitialMarginPerOrder() {
+        this.marginQTY = (currentDeposit * (risk / 100) * LEVERAGE) / (currentPrice);
 
-        //Order order = new Order(type, price); // Создание нового ордера
-        //openOrders.add(order); // Добавление ордера в список открытых ордеров
+        // Округляем marginQTY в меньшую сторону до одного знака после запятой
+        this.marginQTY = Math.floor(this.marginQTY * 10) / 10.0;
+
+//        if(marginQTY < minTradingQty){
+//            this.marginQTY = minTradingQty;
+//        }
+
+        // Проверяем, если marginQTY меньше minTradingQty не более чем на 10%
+        if (this.marginQTY < this.minTradingQty) {
+            if (this.marginQTY >= this.minTradingQty * 0.9) {
+                this.marginQTY = this.minTradingQty;
+                strategyAllowed = true; // Разрешаем стратегии работать
+            } else {
+                strategyAllowed = false; // Запрещаем стратегии работать
+            }
+        }
     }
 
-    // Метод для закрытия позиции
-    public void closePosition(Order order, double price) {
-        if (!strategyAllowed) return; // Проверка флага разрешения стратегии
+    /**
+     * Метод для открытия LONG позиции
+     */
+    private void openLongPosition(Candle candle) {
+        // Пересчет маржи для следующего пула сделок
+        calculateInitialMarginPerOrder();
+        if (currentDeposit < marginPerOrder) {
+            return;
+        }
 
-        //order.close(price); // Закрытие ордера
-        //openOrders.remove(order); // Удаление ордера из списка открытых ордеров
-        //closedOrders.add(order); // Добавление ордера в список закрытых ордеров
+        openOrders++;
+
+        position = new Position(tradingPair, TYPE.LONG, LEVERAGE);
+        Order order = new Order(tradingPair, "buy", candle.getTime(), marginQTY, currentPrice, STATUS.OPEN);
+        position.addOrder(order);
+        orderHistory.add(order);
+    }
+
+    /**
+     * Метод для открытия SHORT позиции
+     */
+    private void openShortPosition(Candle candle) {
+
+        // Пересчет маржи для следующего пула сделок
+        calculateInitialMarginPerOrder();
+
+        if (currentDeposit < marginPerOrder) {
+            return;
+        }
+
+        openOrders++;
+
+        position = new Position(tradingPair, TYPE.SHORT, LEVERAGE);
+        Order order = new Order(tradingPair, "sell", candle.getTime(), marginQTY, currentPrice, STATUS.OPEN);
+        position.addOrder(order);
+        orderHistory.add(order);
+    }
+
+    /**
+     * Метод для закрытия всех SHORT позиций
+     */
+    private void closeShortPosition(Candle candle) {
+        // Закрытие позиции
+        Order order = new Order(tradingPair, "buy", candle.getTime(), marginQTY * openOrders, currentPrice, STATUS.CLOSE);
+        position.closePosition(order);
+        orderHistory.add(order);
+
+        // Обновление депозита
+        double profit = position.getProfit();
+        currentDeposit += profit;
+        //System.out.println("---- Текущий депозит: " + currentDeposit);
+        // Обновление состояния стратегии
+
+        openOrders = 0;
+        positionHistory.add(position);
     }
 }
