74097c0cd1487baaa2f24dce0c981d7c71b280ad
PinBot <lll24la@gmail.com>
2024-07-02 18:43:29 +0300

    Глобальная рестуктуризация кода. Подготовка к реализации финальной версии аутентификации пользователей.
    Повторный коммит

--- src/main/java/com/zmn/pinbotserver/strategyTesting/model/order/STATUS.java
+++
@@ -1,6 +0,0 @@
-package com.zmn.pinbotserver.strategyTesting.model.order;
-
-public enum STATUS {
-    OPEN,
-    CLOSE
-}
--- src/main/java/com/zmn/pinbotserver/strategyTesting/model/order/TYPE.java
+++
@@ -1,6 +0,0 @@
-package com.zmn.pinbotserver.strategyTesting.model.order;
-
-public enum TYPE {
-    LONG,
-    SHORT
-}
