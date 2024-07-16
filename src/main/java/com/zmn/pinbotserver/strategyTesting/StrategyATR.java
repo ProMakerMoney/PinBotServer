24fc8c2525e5fb57627ec07f6ef915bb522d95b6
PinBot <lll24la@gmail.com>
2024-07-16 09:18:01 +0300

    JavaDoc комментарии

--- src/main/java/com/zmn/pinbotserver/strategyTesting/StrategyATR.java
+++ src/main/java/com/zmn/pinbotserver/strategyTesting/StrategyATR.java
@@ -12,6 +12,9 @@ import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StrategyATR {
 
     boolean strategyAllowed = true; // Флаг, разрешающий или запрещающий работу стратегии
@@ -82,7 +85,14 @@ public class StrategyATR {
     @Getter
     String mode = "free"; // режим по умолчанию
 
-    // Конструктор стратегии
+    /**
+     * Конструктор стратегии ATR.
+     *
+     * @param strategyParams Параметры стратегии.
+     * @param initialDeposit Начальный депозит.
+     * @param minTradingQty Минимальное количество для торговли.
+     * @param risk Уровень риска.
+     */
     public StrategyATR(StrategyParamsATR strategyParams, double initialDeposit, double minTradingQty, double risk) {
         this.tradingPair = strategyParams.getCoinName();
         this.LEVERAGE = strategyParams.getLEVERAGE();
@@ -100,6 +110,9 @@ public class StrategyATR {
         calculateInitialMarginPerOrder();
     }
 
+    /**
+     * Метод для расчета маржи на сделку.
+     */
     private void calculateInitialMarginPerOrder() {
         this.marginQTY = (currentDeposit * (risk / 100) * LEVERAGE) / (MAXOrders * currentPrice);
         this.marginQTY = Math.floor(this.marginQTY * 10) / 10.0;
@@ -114,6 +127,15 @@ public class StrategyATR {
         }
     }
 
+    /**
+     * Метод для расчета AlphaTrend индикатора.
+     *
+     * @param low Текущая минимальная цена.
+     * @param high Текущая максимальная цена.
+     * @param atr Значение ATR.
+     * @param coeff Множитель для AlphaTrend.
+     * @param period Период для AlphaTrend.
+     */
     public void calcAlphaTrend(double low, double high, double atr, double coeff, int period) {
         double upT = low - atr * coeff;
         double downT = high + atr * coeff;
@@ -129,18 +151,46 @@ public class StrategyATR {
         }
     }
 
+    /**
+     * Метод для определения пересечения снизу.
+     *
+     * @param current Текущее значение.
+     * @param previous Предыдущее значение.
+     * @return true, если произошло пересечение снизу, иначе false.
+     */
     public boolean taCrossover(double current, double previous) {
         return current > previous;
     }
 
+    /**
+     * Метод для определения пересечения сверху.
+     *
+     * @param current Текущее значение.
+     * @param previous Предыдущее значение.
+     * @return true, если произошло пересечение сверху, иначе false.
+     */
     public boolean taCrossunder(double current, double previous) {
         return current < previous;
     }
 
+    /**
+     * Метод для расчета среднего значения High, Low и Close (HLC3).
+     *
+     * @param candle Объект свечи.
+     * @return Среднее значение HLC3.
+     */
     public double hlc3(Candle candle) {
         return (candle.getHigh() + candle.getLow() + candle.getClose()) / 3;
     }
 
+    /**
+     * Метод для расчета Money Flow Index (MFI).
+     *
+     * @param candleHistory Список свечей.
+     * @param period Период для MFI.
+     * @return Значение MFI.
+     * @throws IllegalArgumentException если недостаточно данных для расчета MFI.
+     */
     public double taMFI(List<Candle> candleHistory, int period) {
         if (candleHistory.size() < period + 1) {
             throw new IllegalArgumentException("Недостаточно данных для расчета MFI.");
@@ -163,6 +213,14 @@ public class StrategyATR {
         return 100 - (100 / (1 + moneyFlowRatio));
     }
 
+    /**
+     * Метод для расчета Average True Range (ATR).
+     *
+     * @param candleHistory Список свечей.
+     * @param ATR_length Период для ATR.
+     * @return Значение ATR.
+     * @throws IllegalArgumentException если недостаточно данных для расчета ATR.
+     */
     public double calculateATR(List<Candle> candleHistory, int ATR_length) {
         if (candleHistory.size() < ATR_length) {
             throw new IllegalArgumentException("Недостаточно данных для расчета ATR.");
@@ -180,12 +238,19 @@ public class StrategyATR {
         return atr / ATR_length;
     }
 
+    /**
+     * Метод для расчета EMA.
+     *
+     * @param newValue Новое значение для включения в расчет.
+     * @param period Период EMA.
+     * @return Рассчитанное значение EMA.
+     */
     public double calculateEMA(double newValue, int period) {
         if (emaValues.isEmpty()) {
             emaValues.add(newValue);
             return newValue;
         } else {
-            double lastEma = emaValues.getLast();
+            double lastEma = emaValues.get(emaValues.size() - 1);
             double alpha = 2.0 / (period + 1);
             double ema = alpha * newValue + (1 - alpha) * lastEma;
             emaValues.add(ema);
@@ -193,6 +258,11 @@ public class StrategyATR {
         }
     }
 
+    /**
+     * Метод для расчета CCI (Commodity Channel Index).
+     *
+     * @return Рассчитанное значение CCI.
+     */
     public double calculateCCI() {
         int period = CCI_PERIOD;
         if (candleHistory.size() < period) {
@@ -208,9 +278,14 @@ public class StrategyATR {
             sumMeanDeviation += Math.abs(candleHistory.get(i).getTypicalPrice() - meanTypicalPrice);
         }
         double meanDeviation = sumMeanDeviation / period;
-        return (candleHistory.getLast().getTypicalPrice() - meanTypicalPrice) / (0.015 * meanDeviation);
+        return (candleHistory.get(candleHistory.size() - 1).getTypicalPrice() - meanTypicalPrice) / (0.015 * meanDeviation);
     }
 
+    /**
+     * Метод, вызываемый при обновлении цены.
+     *
+     * @param candle Новая свеча.
+     */
     public void onPriceUpdate(Candle candle) {
         if (!candleHistory.contains(candle)) {
             currentPrice = candle.getClose();
@@ -222,7 +297,7 @@ public class StrategyATR {
             return;
         }
         if (candleHistory.size() > MINIMUM_CANDLES) {
-            candleHistory.removeFirst();
+            candleHistory.remove(0);
         }
         double atr = calculateATR(candleHistory, ATR_length);
         calcAlphaTrend(candle.getLow(), candle.getHigh(), atr, coeff, ATR_length);
@@ -252,18 +327,33 @@ public class StrategyATR {
         }
     }
 
+    /**
+     * Метод для проверки готовности открытия первой LONG позиции.
+     *
+     * @param cci Текущее значение CCI.
+     */
     private void checkFirstLongReady(double cci) {
         if (cci < lowerBound && !longIsOpen && !longIsReadyAVG && openOrders == 0 && !longIsReady) {
             longIsReady = true;
         }
     }
 
+    /**
+     * Метод для проверки готовности открытия первой SHORT позиции.
+     *
+     * @param cci Текущее значение CCI.
+     */
     private void checkFirstShortReady(double cci) {
         if (cci > upperBound && !shortIsOpen && !shortIsReadyAVG && openOrders == 0 && !shortIsReady) {
             shortIsReady = true;
         }
     }
 
+    /**
+     * Метод для проверки готовности усреднения LONG позиции.
+     *
+     * @param cci Текущее значение CCI.
+     */
     private void checkLongAverageReady(double cci) {
         if (direction == TYPE.LONG && longIsOpen && openOrders <= MAXOrders && cci > lowerBound) {
             cciLongRollback = true;
@@ -273,6 +363,11 @@ public class StrategyATR {
         }
     }
 
+    /**
+     * Метод для проверки готовности усреднения SHORT позиции.
+     *
+     * @param cci Текущее значение CCI.
+     */
     private void checkShortAverageReady(double cci) {
         if (direction == TYPE.SHORT && shortIsOpen && openOrders <= MAXOrders && cci < upperBound) {
             cciShortRollback = true;
@@ -282,34 +377,79 @@ public class StrategyATR {
         }
     }
 
+    /**
+     * Метод для проверки условий открытия первой LONG позиции.
+     *
+     * @param cci Текущее значение CCI.
+     * @param ema Текущее значение EMA.
+     * @return true, если условия выполнены, иначе false.
+     */
     private boolean canOpenFirstLongPosition(double cci, double ema) {
         return direction == TYPE.LONG && longIsReady && cci > ema && !longIsReadyAVG && !longIsOpen && cci <= upperBound && !shortIsOpen && !shortIsReadyAVG;
     }
 
+    /**
+     * Метод для проверки условий открытия первой SHORT позиции.
+     *
+     * @param cci Текущее значение CCI.
+     * @param ema Текущее значение EMA.
+     * @return true, если условия выполнены, иначе false.
+     */
     private boolean canOpenFirstShortPosition(double cci, double ema) {
         return direction == TYPE.SHORT && shortIsReady && cci < ema && !shortIsReadyAVG && !shortIsOpen && cci >= lowerBound && !longIsOpen && !longIsReadyAVG;
     }
 
+    /**
+     * Метод для проверки условий усреднения LONG позиции.
+     *
+     * @param cci Текущее значение CCI.
+     * @param ema Текущее значение EMA.
+     * @return true, если условия выполнены, иначе false.
+     */
     private boolean canAverageLongPosition(double cci, double ema) {
         return direction == TYPE.LONG && longIsReadyAVG && cci > ema && currentPrice < last_long_price && longIsOpen && !shortIsOpen && !shortIsReadyAVG;
     }
 
+    /**
+     * Метод для проверки условий усреднения SHORT позиции.
+     *
+     * @param cci Текущее значение CCI.
+     * @param ema Текущее значение EMA.
+     * @return true, если условия выполнены, иначе false.
+     */
     private boolean canAverageShortPosition(double cci, double ema) {
         return direction == TYPE.SHORT && shortIsReadyAVG && cci < ema && currentPrice > last_short_price && !longIsOpen && shortIsOpen;
     }
 
+    /**
+     * Метод для проверки условий закрытия LONG позиции.
+     *
+     * @param cci Текущее значение CCI.
+     * @return true, если условия выполнены, иначе false.
+     */
     private boolean canCloseLongPosition(double cci) {
         double liquidationLevelPer = 100.0 / LEVERAGE; // Уровень ликвидации в процентах
         return (longIsOpen && (currentPrice <= last_long_price * (1 - liquidationLevelPer / 100)) ||
                 (direction == TYPE.SHORT && cci > upperBound && longIsOpen && openOrders > 0));
     }
 
+    /**
+     * Метод для проверки условий закрытия SHORT позиции.
+     *
+     * @param cci Текущее значение CCI.
+     * @return true, если условия выполнены, иначе false.
+     */
     private boolean canCloseShortPosition(double cci) {
         double liquidationLevelPer = 100.0 / LEVERAGE; // Уровень ликвидации в процентах
         return (shortIsOpen && (currentPrice >= last_short_price * (1 + liquidationLevelPer / 100)) ||
                 (direction == TYPE.LONG && cci < lowerBound && shortIsOpen && openOrders > 0));
     }
 
+    /**
+     * Метод для открытия LONG позиции.
+     *
+     * @param candle Свеча.
+     */
     private void openLongPosition(Candle candle) {
         if ("stop".equals(mode) || "smooth_stop".equals(mode)) {
             return;
@@ -328,6 +468,11 @@ public class StrategyATR {
         longIsOpen = true;
     }
 
+    /**
+     * Метод для усреднения LONG позиции.
+     *
+     * @param candle Свеча.
+     */
     private void averageLongPosition(Candle candle) {
         if (currentDeposit < marginPerOrder) {
             return;
@@ -341,6 +486,11 @@ public class StrategyATR {
         cciLongRollback = false;
     }
 
+    /**
+     * Метод для закрытия LONG позиции.
+     *
+     * @param candle Свеча.
+     */
     private void closeLongPosition(Candle candle) {
         Order order = new Order(tradingPair, "sell", candle.getTime(), marginQTY * openOrders, currentPrice, STATUS.CLOSE);
         position.closePosition(order);
@@ -355,12 +505,17 @@ public class StrategyATR {
         positionHistory.add(position);
     }
 
+    /**
+     * Метод для открытия SHORT позиции.
+     *
+     * @param candle Свеча.
+     */
     private void openShortPosition(Candle candle) {
         if ("stop".equals(mode) || "smooth_stop".equals(mode)) {
             return;
         }
         calculateInitialMarginPerOrder();
-        if (currentDeposit < marginPerOrder  * MAXOrders) {
+        if (currentDeposit < marginPerOrder * MAXOrders) {
             return;
         }
         openOrders++;
@@ -373,6 +528,11 @@ public class StrategyATR {
         shortIsOpen = true;
     }
 
+    /**
+     * Метод для усреднения SHORT позиции.
+     *
+     * @param candle Свеча.
+     */
     private void averageShortPosition(Candle candle) {
         if (currentDeposit < marginPerOrder) {
             return;
@@ -386,6 +546,11 @@ public class StrategyATR {
         cciShortRollback = false;
     }
 
+    /**
+     * Метод для закрытия SHORT позиции.
+     *
+     * @param candle Свеча.
+     */
     private void closeShortPosition(Candle candle) {
         Order order = new Order(tradingPair, "buy", candle.getTime(), marginQTY * openOrders, currentPrice, STATUS.CLOSE);
         position.closePosition(order);
@@ -399,3 +564,4 @@ public class StrategyATR {
         positionHistory.add(position);
     }
 }
+
